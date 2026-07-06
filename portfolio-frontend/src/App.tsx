6fca2e5a2ad8f38afcd2d9d631e1b5660b92a005
iconicconcept <mubaraqadeniyi159@gmail.com>
2026-07-06 11:04:25 +0100

    restructuring5

--- portfolio-frontend/src/App.tsx
+++ portfolio-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Projects from "./pages/Projects";
 import { Routes, Route } from "react-router";
 
 const myText =
-  "I am a seasoned Full-Stack Developer with hands-on experience building high quality, robust and scalable digital solutions. I specialize in Frontend development, turning complex UIs into functional web applications that have real-world impact.";
+  "I am a Full-Stack Developer with hands-on experience building high quality, robust and scalable digital solutions. I focus on Frontend development, turning complex UIs into functional web applications that have real-world impact.";
 
 function App() {
   return (
